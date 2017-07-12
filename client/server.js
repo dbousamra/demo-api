68df7947e56672ddcb311603b72ce3c1fc01f1b2
Dominic Bou-Samra <dominic.bou.samra@gmail.com>
2017-07-12 20:16:37 +1000

    refactor(client-server): Changed how express serves the client

--- client/server.js
+++ client/server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const path = require('path');
 
 // Constants
 const PORT = 3000;
@@ -8,9 +9,8 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello world\n');
-});
+app.use('/', express.static(__dirname + '/dist'));
+
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
