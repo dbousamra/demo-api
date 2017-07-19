c15a79c569a85e694db7a8e9502c4e5810e6bb43
Sebastian Bou-Samra <sebastian.bousamra@hotmail.com>
2017-07-19 16:19:17 +1000

    added lodash to package.json, removed few components, added input tracking on forms and mapped array of 'rounds' we will retrieve from demo when uploaded

--- client/src/components/CreateDemo/CreateDemo.js
+++ client/src/components/CreateDemo/CreateDemo.js
@@ -1,19 +1,29 @@
 
 import React from "react";
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import * as lodash from 'lodash'
 
 class CreateDemo extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
+      rounds: [1,2,3,4,5,6,7],
       playerName: "",
       demoUrl: "",
       roundsOfInterest: [],
       rank: "",
       comments: ""
     }
+    this.handleInput = this.handleInput.bind(this)
     this.handleSubmitDemoRequest = this.handleSubmitDemoRequest.bind(this)
+  }
+
+  handleInput(e, state) {
+    this.setState({
+      [state]: e.target.value
+    })
+    console.log(this.state)
   }
 
   handleSubmitDemoRequest() {
@@ -28,39 +38,39 @@ class CreateDemo extends React.Component {
   }
 
   render() {
+
+    const roundsToRadio = lodash.map(this.state.rounds, (round) => {
+      return (
+        <FormGroup check key={round}>
+          <Label check>
+            <Input type="radio" name="round" />{' '}
+            {round}
+          </Label>
+        </FormGroup>
+      )
+    })
+
+    const roundsOfInterest =
+      <FormGroup tag="fieldset">
+        <legend>Rounds of interest</legend>
+        {roundsToRadio}
+      </FormGroup>
+
+
     return (
       <Form>
         <FormGroup>
-          <Label for="exampleEmail">Email</Label>
-          <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+          <Label for="exampleEmail">Alias</Label>
+          <Input type="text" name="playerName" id="playerName" placeholder="What's your alias?" onChange={(e) => this.handleInput(e, "playerName")}/>
         </FormGroup>
         <FormGroup>
-          <Label for="examplePassword">Password</Label>
-          <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+          <Label for="examplePassword">Demo Url</Label>
+          <Input type="text" name="demoUrl" id="demoUrl" placeholder="Demo url" onChange={(e) => this.handleInput(e, "demoUrl")}/>
         </FormGroup>
+        {roundsOfInterest}
         <FormGroup>
-          <Label for="exampleSelect">Select</Label>
-          <Input type="select" name="select" id="exampleSelect">
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-          </Input>
-        </FormGroup>
-        <FormGroup>
-          <Label for="exampleSelectMulti">Select Multiple</Label>
-          <Input type="select" name="selectMulti" id="exampleSelectMulti" multiple>
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-          </Input>
-        </FormGroup>
-        <FormGroup>
-          <Label for="exampleText">Text Area</Label>
-          <Input type="textarea" name="text" id="exampleText" />
+          <Label for="comments">Comments</Label>
+          <Input type="textarea" name="text" id="comments" onChange={(e) => this.handleInput(e, "comments")}/>
         </FormGroup>
         <FormGroup>
           <Label for="exampleFile">File</Label>
@@ -69,27 +79,6 @@ class CreateDemo extends React.Component {
             This is some placeholder block-level help text for the above input.
             It's a bit lighter and easily wraps to a new line.
           </FormText>
-        </FormGroup>
-        <FormGroup tag="fieldset">
-          <legend>Radio Buttons</legend>
-          <FormGroup check>
-            <Label check>
-              <Input type="radio" name="radio1" />{' '}
-              Option one is this and that—be sure to include why it's great
-            </Label>
-          </FormGroup>
-          <FormGroup check>
-            <Label check>
-              <Input type="radio" name="radio1" />{' '}
-              Option two can be something else and selecting it will deselect option one
-            </Label>
-          </FormGroup>
-          <FormGroup check disabled>
-            <Label check>
-              <Input type="radio" name="radio1" disabled />{' '}
-              Option three is disabled
-            </Label>
-          </FormGroup>
         </FormGroup>
         <Button onClick={this.handleSubmitDemoRequest}>Submit</Button>
       </Form>
